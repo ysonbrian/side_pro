9c972ac2aed0767fc8de23f378f92752b7c2dcea
ysonbrian <znlsakxls@gmail.com>
2021-02-03 23:01:00 +0900

    Added Dark and White Mode

--- Day3/script.js
+++ Day3/script.js
@@ -1,6 +1,9 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const dark = document.getElementById('dark');
+const body = document.querySelector('body');
+const content = document.querySelector('.content');
 
 open.addEventListener('click', () => {
 	container.classList.add('show-nav');
@@ -9,3 +12,35 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
 	container.classList.remove('show-nav');
 });
+
+dark.addEventListener('click', (e) => {
+	if (dark.children[0] === e.target) {
+		darkColor();
+	} else {
+		whiteColor();
+	}
+});
+
+function darkColor() {
+	dark.children[0].classList.toggle('hidden');
+	dark.children[1].classList.toggle('hidden');
+	body.style.backgroundColor = 'black';
+	container.style.backgroundColor = 'black';
+	content.childNodes.forEach((child, i) => {
+		if (i % 2 !== 0) {
+			child.style.color = 'white';
+		}
+	});
+}
+
+function whiteColor() {
+	dark.children[0].classList.toggle('hidden');
+	dark.children[1].classList.toggle('hidden');
+	body.style.backgroundColor = '#333';
+	container.style.backgroundColor = '#fff';
+	content.childNodes.forEach((child, i) => {
+		if (i % 2 !== 0) {
+			child.style.color = '#222';
+		}
+	});
+}
