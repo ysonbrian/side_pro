1ad95e94d72366482c99a408ac8fe8d64e8cc3f1
ysonbrian <znlsakxls@gmail.com>
2021-02-11 22:50:49 +0900

    Day 10 Finished

---
+++ Day10/script.js
@@ -0,0 +1,33 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+	const res = await fetch('https://icanhazdadjoke.com/', config);
+
+	const data = await res.json();
+	jokeEl.innerHTML = data.joke;
+}
+
+// USING .then()
+// function generateJoke() {
+// 	const config = {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	};
+// 	fetch('https://icanhazdadjoke.com/', config)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			jokeEl.innerHTML = data.joke;
+// 		});
+// }
